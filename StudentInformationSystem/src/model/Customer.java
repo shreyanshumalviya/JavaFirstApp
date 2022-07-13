d44fe59afef2b5fc0c4a1d10f8fc2ebe9c367d03
Shreyanshu <shreyanshumalviya@gmail.com>
2022-07-13 12:26:03 +0530

    Added customer model

---
+++ StudentInformationSystem/src/model/Customer.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Customer {
+	int customerId;
+	String customerName;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	
+}
