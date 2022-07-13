8f20c6853519c0e827a969feee2c036813b8f801
Shreyanshu <shreyanshumalviya@gmail.com>
2022-07-13 15:25:53 +0530

    branch

--- StudentInformationSystem/src/model/Customer.java
+++ StudentInformationSystem/src/model/Customer.java
@@ -3,18 +3,25 @@ package model;
 public class Customer {
 	int customerId;
 	String customerName;
+	int age;
+	
 	public int getCustomerId() {
 		return customerId;
-	}
-	public String getCustomerName() {
-		return customerName;
 	}
 	public void setCustomerId(int customerId) {
 		this.customerId = customerId;
 	}
+	public String getCustomerName() {
+		return customerName;
+	}
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
-	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	
 }
